5d132d1284941910e1993fdaca9accc3145a740b
msansm1 <msansm1@dizalch.fr>
2020-05-01 14:42:34 +0200

    Clean code + unused dependencies

--- src/bzh/msansm1/klockAlarm/Application.kt
+++ src/bzh/msansm1/klockAlarm/Application.kt
@@ -8,13 +8,10 @@ import io.ktor.application.install
 import io.ktor.features.*
 import io.ktor.freemarker.FreeMarker
 import io.ktor.freemarker.FreeMarkerContent
-import io.ktor.http.HttpStatusCode
 import io.ktor.http.content.resources
 import io.ktor.http.content.static
 import io.ktor.jackson.jackson
-import io.ktor.locations.Location
-import io.ktor.locations.Locations
-import io.ktor.locations.get
+import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
@@ -27,9 +24,6 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun Application.module(testing: Boolean = false) {
     install(FreeMarker) {
         templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
-    }
-
-    install(Locations) {
     }
 
     install(Compression) {
@@ -60,16 +54,6 @@ fun Application.module(testing: Boolean = false) {
             resources("static")
         }
 
-        get<MyLocation> {
-            call.respondText("Location: name=${it.name}, arg1=${it.arg1}, arg2=${it.arg2}")
-        }
-        // Register nested routes
-        get<Type.Edit> {
-            call.respondText("Inside $it")
-        }
-        get<Type.List> {
-            call.respondText("Inside $it")
-        }
         get("/api/v1") {
             val alarms = getAlarms();
             call.respond(alarms)
@@ -84,31 +68,9 @@ fun Application.module(testing: Boolean = false) {
             }
 
         }
-
-        get("/json/jackson") {
-            call.respond(mapOf("hello" to "world"))
-        }
     }
 }
 
-data class IndexData(val items: List<Int>)
-
-@Location("/location/{name}")
-class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
-
-@Location("/type/{name}")
-data class Type(val name: String) {
-    @Location("/edit")
-    data class Edit(val type: Type)
-
-    @Location("/list/{page}")
-    data class List(val type: Type, val page: Int)
-}
-
-class AuthenticationException : RuntimeException()
-class AuthorizationException : RuntimeException()
-
-@Location("/api/v1/")
 data class Alarm(
     val id: String,
     val hour: String,
